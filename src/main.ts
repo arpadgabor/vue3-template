9379a340e04f1c22888c6b9f00dafc0c68535e36
Arpad Gabor <hello@arpadgabor.com>
2023-10-25 09:42:07 +0000

    Update 4 files

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import { createApp } from 'vue';
-import './style.css';
-import App from './App.vue';
-import { VueQueryPlugin } from '@tanstack/vue-query';
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import { VueQueryPlugin } from "@tanstack/vue-query";
 
-createApp(App).use(VueQueryPlugin).mount('#app');
+createApp(App).use(VueQueryPlugin).mount("#app");
+export const x = 1;
